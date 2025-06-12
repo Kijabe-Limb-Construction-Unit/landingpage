09bed165dfb041b713ddf36aca2e97e0470aaf9c
uwasejeannine <jeannineuwasee@gmail.com>
2025-06-12 14:49:28 +0200

    add contact page

--- app/contact-us/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/contact-us/page.tsx
-export default function ContactUsPage() {
-  return (
-    <div>
-      <h1>Contact Us</h1>
-    </div>
-  );
-}
---
+++ app/contact/page.tsx
@@ -0,0 +1,25 @@
+// app/contact-us/page.tsx
+export default function ContactUsPage() {
+  return (
+    <div className="bg-white">
+      {/* Contact Us Section */}
+      <div className="bg-[#4FB29E] py-16">
+        <div className="max-w-4xl mx-auto px-8 flex justify-center">
+          <div className="w-full max-w-md text-white">
+            <h1 className="text-4xl mb-6"> <span className="font-bold"> contact</span> us  </h1>
+            
+            <div className="mb-8">
+              <h2 className="text-md font-bold tracking-widest mb-4">KIJABE LIMB RECONSTRUCTION UNIT</h2>
+              
+              <div className="space-y-1 text-sm">
+                <p>E-mail: mbona@oneitec.church</p>
+                <p>WhatsApp: +447-503-5579</p>
+                <p>Mobile: +254-715-857-94</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
