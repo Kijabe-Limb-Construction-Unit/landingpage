2ad0b279a4ff7ec73f0568cd28291a513896d83e
uwasejeannine <jeannineuwasee@gmail.com>
2025-07-18 17:41:22 +0200

    add

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,13 @@
 export default function ContactUsPage() {
   return (
     <div className="bg-white">
-      <div className="bg-[#4FB29E] py-16">
+      <div className="bg-[#4FB29E] py-4">
         <div className="max-w-4xl mx-auto px-8 flex justify-center">
           <div className="w-full max-w-md text-white">
-            <h1 className="text-4xl mb-6"> <span className="font-bold"> contact</span> us  </h1>
+            <h1 className="text-4xl mb-2"> <span className="font-bold"> contact</span> us  </h1>
             
-            <div className="mb-8">
-              <h2 className="text-md font-bold tracking-widest mb-4">KIJABE LIMB RECONSTRUCTION UNIT</h2>
+            <div className="mb-2">
+              <h2 className="text-md font-bold tracking-widest mb-2">KIJABE LIMB RECONSTRUCTION UNIT</h2>
               
               <div className="space-y-1 text-sm">
                 <p>E-mail: mbona@oneitec.church</p>
